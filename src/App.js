8de8329484defcb1c12908260d516fe3047dd48c
Cristian Osorio <cristian.osorio@globant.com>
2019-11-25 20:30:42 -0300

    componentes separados

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './components/Navigation';
+
+function App() {
+  return (
+    <div className="App">
+        <Navigation title="Hola" />
+        <Navigation title="Chao" />
+        <img src={logo} className="App-logo" alt="logo" />
+    </div>
+  );
+}
+
+export default App;
