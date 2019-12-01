32183d6485fc8c3f928f1082598ed0afd423d44b
Cristian Osorio <cristian.osorio@globant.com>
2019-12-01 17:50:40 -0300

    se agrega tareas y componente formulario

--- src/App.js
+++ src/App.js
@@ -1,16 +1,86 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// data
+import { todos } from './todos.json';
+
+// subcomponent
+import TodoForm from './components/TodoForm';
 import Navigation from './components/Navigation';
 
-function App() {
+class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      title: 'Aplicacion de tareas',
+      ntareas: 10,
+      todos
+    }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
+  }
+
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  render() {
+  
+  const todos = this.state.todos.map((todo, i) => {
   return (
-    <div className="App">
-        <Navigation title="Hola" />
-        <Navigation title="Chao" />
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="col-md-4">
+      <div className="card mt-4">
+        <div className="card-header">
+          <h3>{todo.title}</h3>
+          <span className="badge badge-pill badge-danger ml-2">
+            {todo.priority}
+          </span>
+        </div>
+        <div className="card-body">
+          <p>{todo.description}</p>
+        </div>
+        <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+        </div>
+      </div>
     </div>
   );
+  })
+  
+    return (
+    <div className="App">
+        <Navigation title="Hola" todos={todos} />
+        <Navigation title="Chao" todos={todos}/>
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col md-3">
+            <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
+            </div>
+            <div className="col md-9">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  );
+}
 }
 
 export default App;
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 class Navigation extends Component {
 render() {
+    console.log("hola navegador");
+    console.log(this.props.todos);
     return (
         <nav className="navbar navbar-dark bg-dark">
         <a href="" className="text-white">
